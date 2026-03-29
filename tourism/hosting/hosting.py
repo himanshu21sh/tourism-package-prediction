04b78e135d960fa6cc51dad01b3237eefbe7791b
himanshu21sh <himanshu.21.sh@gmail.com>
2026-03-29 10:50:00 +0000

    Updated files for tourism ml ops pipeline

--- tourism/hosting/hosting.py
+++ tourism/hosting/hosting.py
@@ -2,9 +2,23 @@ from huggingface_hub import HfApi
 import os
 
 api = HfApi(token=os.getenv("HF_TOKEN"))
+
+repo_id="himanshu21sh/tourism-package-prediction",
+repo_type="space"
+
+# Step 1: Check if the space exists
+try:
+    api.repo_info(repo_id=repo_id, repo_type=repo_type)
+    print(f"Model Space '{repo_id}' already exists")
+except RepositoryNotFoundError:
+    print(f"Model Space '{repo_id}' not found. Creating new space...")
+    create_repo(repo_id=repo_id, repo_type=repo_type, private=False)
+    print(f"Model Space '{repo_id}' created.")
+
+
 api.upload_folder(
     folder_path="tourism/deployment",
-    repo_id="himanshu21sh/tourism-package-prediction",
-    repo_type="space",
+    repo_id=repo_id,
+    repo_type=repo_type,
     path_in_repo="" 
     )
